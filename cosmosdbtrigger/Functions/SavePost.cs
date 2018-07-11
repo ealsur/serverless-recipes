c23d87bf04b1adf792cbe275883b659fecc97d14
Matias Quaranta <ealsur@hotmail.com>
2018-07-11 10:30:25 -0300

    Fixing serialization issue

--- cosmosdbtrigger/Functions/SavePost.cs
+++ cosmosdbtrigger/Functions/SavePost.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using cosmosdbtrigger.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
 
 namespace cosmosdbtrigger
 {
@@ -18,9 +19,13 @@ namespace cosmosdbtrigger
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] MessagePayload post, 
             [DocumentDB("%CosmosDBDatabase%", "%CosmosDBCollection%", Id = "id", ConnectionStringSetting = "AzureCosmosDBConnectionString")] out dynamic document)
         {
-            document = new { id = Guid.NewGuid(), message = post.message };
-
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            string newId = Guid.NewGuid().ToString();
+            document = new { id = newId, message = post.message };
+            string result = JsonConvert.SerializeObject(new { id = newId });
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(result, Encoding.UTF8, "application/json")
+            };
         }
     }
 }
