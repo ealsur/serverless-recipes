2d16a449c18609b1bb21353dec7262e12293e091
Matias Quaranta <ealsur@hotmail.com>
2018-11-12 10:42:22 -0300

    Adding multimasteroutput sample

---
+++ cosmosdboutputbindings/src/HttpTriggerToAsyncCollectorMultiMaster.cs
@@ -0,0 +1,56 @@
+using cosmosdboutputbindings.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace cosmosdboutputbindings
+{
+    /// <summary>
+    /// This sample binds a custom array of <see cref="MyClass"/> objects from the HTTP Trigger body and uses the Cosmos DB IAsyncCollector to save them.
+    /// </summary>
+    /// <remarks>Sample payload is:
+    /// {
+    ///     "id": "SomeId", /* optional */    
+    ///     "name": "SomeName",
+    ///     "city": "SomeCity"
+    /// }
+    /// </remarks>
+    public static class HttpTriggerToAsyncCollectorMultiMaster
+    {
+        [FunctionName("HttpTriggerToAsyncCollectorMultiMaster")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, 
+            [CosmosDB(databaseName: "%CosmosDBDatabase%",
+                collectionName: "%CosmosDBCollection%",
+                ConnectionStringSetting = "CosmosDBConnectionString",
+                UseMultipleWriteLocations = true,
+                PreferredLocations = "Central US"
+            )] IAsyncCollector<MyClass> documentsToSave,
+            ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            MyClass[] inputDocuments = JsonConvert.DeserializeObject<MyClass[]>(requestBody);
+            if (inputDocuments == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+
+            foreach (MyClass inputDocument in inputDocuments)
+            {
+                await documentsToSave.AddAsync(inputDocument);
+                log.LogInformation(inputDocument.id);
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.Created);
+        }
+    }
+}
